38b2aa6f5c7c122a12e02548e49ce8c45cd34fa6
Ney Adrian <neyadrian2018@gmail.com>
2026-02-18 19:42:27 -0300

    corrigindo erro em votar

--- src/src/SistemaVotação/Urna.java
+++ src/src/SistemaVotação/Urna.java
@@ -8,9 +8,9 @@ public class Urna {
     private ArrayList<Candidato> listaCandidatos;
 
     public void votar(int numeroCandidato) {
-        for (Candidato : listaCandidatos) {
-            if (Candidato.getNumero() == numeroCandidato) {
-                Candidato.setVotos(Candidato.getVotos() + 1);
+        for (Candidato candidato : listaCandidatos) {
+            if (candidato.getNumero() == numeroCandidato) {
+                candidato.setVotos(candidato.getVotos() + 1);
                 totalVotosGeral++;
                 break;
             }
