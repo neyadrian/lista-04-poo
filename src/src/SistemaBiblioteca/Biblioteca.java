a081d2464f0b3acdec7a7856b7ffdf7a06218cef
Ney Adrian <neyadrian2018@gmail.com>
2026-02-19 19:17:46 -0300

    método emprestar livro em biblioteca

--- src/src/SistemaBiblioteca/Biblioteca.java
+++ src/src/SistemaBiblioteca/Biblioteca.java
@@ -14,5 +14,25 @@ public class Biblioteca {
 
     public void adicionarLivro(Livro livro) {
         livros.add(livro);
+        totalLivrosDisponiveis++;
     }
+
+    public void emprestarLivros(String titulo) {
+        for (Livro livro : livros) {
+            if(livro.getTitulo().equalsIgnoreCase(titulo)) {
+                if(livro.isDisponivel()) {
+                    livro.setDisponivel(false);
+                    totalLivrosDisponiveis--;
+                    totalLivrosEmprestados++;
+                    System.out.println("Livro emprestado com sucesso!");
+                } else {
+                    System.out.println("Livro indisponível para empréstimo.");
+                }
+                return;
+            }
+        }
+        System.out.println("Livro não encontrado.");
+    }
+
+
 }
