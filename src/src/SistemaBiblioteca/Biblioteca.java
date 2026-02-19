a7fafd277a139871866c7fdf4c05573b7ad7e76c
Ney Adrian <neyadrian2018@gmail.com>
2026-02-19 16:07:31 -0300

    adicionando métodos em biblioteca

--- src/src/SistemaBiblioteca/Biblioteca.java
+++ src/src/SistemaBiblioteca/Biblioteca.java
@@ -1,4 +1,18 @@
 package SistemaBiblioteca;
 
+import java.util.ArrayList;
+
 public class Biblioteca {
+
+    private ArrayList<Livro> livros;
+    public static int totalLivrosEmprestados;
+    public static int totalLivrosDisponiveis;
+
+    public Biblioteca() {
+        livros = new ArrayList<>();
+    }
+
+    public void adicionarLivro(Livro livro) {
+        livros.add(livro);
+    }
 }
