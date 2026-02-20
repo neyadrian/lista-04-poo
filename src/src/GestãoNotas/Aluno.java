f4bf840e7709cc9094837b57ed5359374112a1a7
Ney Adrian <neyadrian2018@gmail.com>
2026-02-20 15:36:24 -0300

    criando constutor e getters

---
+++ src/src/GestãoNotas/Aluno.java
@@ -0,0 +1,20 @@
+package GestãoNotas;
+
+public class Aluno {
+
+    private String nome;
+    private double nota;
+
+    public Aluno(String nome, double nota) {
+        this.nome = nome;
+        this.nota = nota;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+}
---
+++ src/src/GestãoNotas/Main.java
@@ -0,0 +1,7 @@
+package GestãoNotas;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
