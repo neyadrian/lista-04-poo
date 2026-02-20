e7430e41a4a4e5c48bf7787c9d16edf873f6dad5
Ney Adrian <neyadrian2018@gmail.com>
2026-02-20 20:07:16 -0300

    construtor e getters em aluno

--- src/src/SistemaAcademico/Aluno.java
+++ src/src/SistemaAcademico/Aluno.java
@@ -5,4 +5,22 @@ public class Aluno {
     private String matricula;
     private String nome;
     private String email;
+
+    public Aluno(String matricula, String nome, String email) {
+        this.matricula = matricula;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
