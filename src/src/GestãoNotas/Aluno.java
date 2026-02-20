809c904f021bd5d3a89e0f26a03f6202a1b4a2e4
Ney Adrian <neyadrian2018@gmail.com>
2026-02-20 15:40:13 -0300

    update

--- src/src/GestãoNotas/Aluno.java
+++ src/src/GestãoNotas/Aluno.java
@@ -1,13 +1,17 @@
 package GestãoNotas;
 
+import java.util.ArrayList;
+
 public class Aluno {
 
+    ArrayList<Aluno> alunos;
     private String nome;
     private double nota;
 
     public Aluno(String nome, double nota) {
         this.nome = nome;
         this.nota = nota;
+        alunos.add(this);
     }
 
     public String getNome() {
@@ -17,4 +21,11 @@ public class Aluno {
     public double getNota() {
         return nota;
     }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+
+
 }
