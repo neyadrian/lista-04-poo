43cd6c8dcfa8f22aff61f5e7cfa07b1826db54c6
Ney Adrian <neyadrian2018@gmail.com>
2026-02-20 20:10:40 -0300

    construtor e getters em professor

--- src/src/SistemaAcademico/Professor.java
+++ src/src/SistemaAcademico/Professor.java
@@ -1,4 +1,26 @@
 package SistemaAcademico;
 
 public class Professor {
+
+    private String matricula;
+    private String nome;
+    private String email;
+
+    public Professor(String matricula, String nome, String email) {
+        this.matricula = matricula;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
