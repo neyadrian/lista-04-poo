66dca4085fc99cf32f31fca95165cb804c6152dd
Ney Adrian <neyadrian2018@gmail.com>
2026-02-20 19:15:04 -0300

    constutor do estoque da css

--- src/src/SistemaConcessionaria/Estoque.java
+++ src/src/SistemaConcessionaria/Estoque.java
@@ -1,4 +1,14 @@
 package SistemaConcessionaria;
 
+import java.util.ArrayList;
+
 public class Estoque {
+
+    private ArrayList<Carro> carros;
+
+    public Estoque() {
+        carros = new ArrayList<>();
+    }
+
+
 }
--- src/src/SistemaConcessionaria/Main.java
+++ src/src/SistemaConcessionaria/Main.java
@@ -1,4 +1,7 @@
 package SistemaConcessionaria;
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
