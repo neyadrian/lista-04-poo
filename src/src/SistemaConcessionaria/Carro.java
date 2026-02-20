2f1b9d95290e88394b7d1ca857357716c91eeaca
Ney Adrian <neyadrian2018@gmail.com>
2026-02-20 19:28:40 -0300

    construtor de carro

--- src/src/SistemaConcessionaria/Carro.java
+++ src/src/SistemaConcessionaria/Carro.java
@@ -1,4 +1,12 @@
 package SistemaConcessionaria;
 
 public class Carro {
+
+    private String nome;
+    private double preco;
+
+    public Carro(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
 }
